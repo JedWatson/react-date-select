bc8c8679aef56e1b7af1acf545c92b6bfa8a7b73
Jed Watson <jed.watson@me.com>
2015-04-20 15:19:15 +1000

    Initial build

---
+++ dist/react-date-select.js
@@ -0,0 +1,27 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.DateSelect = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+'use strict';
+
+var _ = (typeof window !== "undefined" ? window._ : typeof global !== "undefined" ? global._ : null),
+    React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null),
+    classNames = (typeof window !== "undefined" ? window.classNames : typeof global !== "undefined" ? global.classNames : null);
+
+var DateSelect = React.createClass({
+
+	displayName: 'DateSelect',
+
+	render: function render() {
+		return React.createElement(
+			'div',
+			null,
+			'(select a date)'
+		);
+	}
+
+});
+
+module.exports = DateSelect;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1])(1)
+});
---
+++ example/dist/example.js
@@ -0,0 +1,19 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*
+Note: ESLint is currently misreporting unused / undeclared variables for JSX.
+These errors can be ignored until the bug has been fixed.
+ */
+
+'use strict';
+
+var React = require('react'),
+    DateSelect = require('react-date-select');
+
+React.render(React.createElement(
+	'div',
+	null,
+	'Date Select:',
+	React.createElement(DateSelect, null)
+), document.getElementById('example'));
+
+},{"react":undefined,"react-date-select":undefined}]},{},[1]);
