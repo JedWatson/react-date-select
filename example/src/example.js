9742614326bba85f09f92b2b010e997e85b499a4
Jed Watson <jed.watson@me.com>
2015-04-20 18:40:52 +1000

    Updating examples

--- example/src/example.js
+++ example/src/example.js
@@ -10,23 +10,28 @@ var DateSelectExamples = React.createClass({
 	displayName: 'DateSelectExamples',
 	getInitialState() {
 		return {
-			
+			singleDateValue: new Date(),
+			multiDateValue1: [new Date(), new Date()],
+			multiDateValue2: [new Date(), new Date()]
 		};
+	},
+	onDateChange(key, value) {
+		this.setState({ key: value });
 	},
 	render() {
 		return (
 			<div>
 				<h3>Day Select</h3>
 				<p>Pick a single date</p>
-				<DateSelect isOpen={this.state.dateSelectIsOpen} onChange={this.toggleDateSelect} />
+				<DateSelect value={this.state.singleDateValue} onChange={this.onDateChange.bind(this, 'singleDateValue')} />
 				
 				<h3>Multi Select</h3>
 				<p>Pick a start and end date</p>
-				<DateSelect isOpen={this.state.multiSelectIsOpen} onChange={this.toggleMultiSelect} isMulti buttonLabel="Launch range picker" />
+				<DateSelect value={this.state.multiDateValue1} onChange={this.onDateChange.bind(this, 'multiDateValue1')} isMulti buttonLabel="Launch range picker" />
 				
 				<h3>Multi Select with Ranges</h3>
 				<p>Pick a start and end date, with the option to use predefined ranges.</p>
-				<DateSelect isOpen={this.state.rangeSelectIsOpen} onChange={this.toggleRangeSelect} isMulti showPredefinedRanges buttonLabel="Launch range picker (with default ranges)" />
+				<DateSelect value={this.state.multiDateValue2} onChange={this.onDateChange.bind(this, 'multiDateValue2')} isMulti showPredefinedRanges buttonLabel="Launch range picker (with default ranges)" />
 			</div>
 		);
 	}
