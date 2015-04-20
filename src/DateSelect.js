6f3b49e52d04d1dd464777116e93fd72054bec59
Jed Watson <jed.watson@me.com>
2015-04-20 15:16:36 +1000

    Adding Component

---
+++ src/DateSelect.js
@@ -0,0 +1,15 @@
+var _ = require('lodash'),
+	React = require('react'),
+	classNames = require('classnames');
+
+var DateSelect = React.createClass({
+
+	displayName: 'DateSelect',
+
+	render () {
+		return <div>(select a date)</div>;
+	}
+
+});
+
+module.exports = DateSelect;
