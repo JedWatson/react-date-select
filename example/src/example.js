8bfd99d9d8b355e7efa38b833d9331236a90860a
Joss Mackison <joss.mackison@gmail.com>
2015-05-06 17:08:18 +1000

    tidy up the example styles

--- example/src/example.js
+++ example/src/example.js
@@ -21,13 +21,13 @@ var DateSelectExamples = React.createClass({
 	render() {
 		return (
 			<div>
-				<h3>Pick a single date</h3>
+				<p>Pick a single date</p>
 				<DateSelect value={this.state.singleDateValue} onChange={this.onDateChange.bind(this, 'singleDateValue')} />
 				
-				<h3>Pick a start and end date</h3>
+				<p>Pick a start and end date</p>
 				<DateSelect value={this.state.multiDateValue1} onChange={this.onDateChange.bind(this, 'multiDateValue1')} isMulti buttonLabel="Launch range picker" />
 				
-				<h3>Pick a start and end date, with the option to use predefined ranges.</h3>
+				<p>Pick a start and end date, with the option to use predefined ranges.</p>
 				<DateSelect value={this.state.multiDateValue2} onChange={this.onDateChange.bind(this, 'multiDateValue2')} isMulti showPredefinedRanges buttonLabel="Launch range picker (with default ranges)" />
 			</div>
 		);
