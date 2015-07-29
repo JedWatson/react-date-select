4da0985c4220996f504cbfe04c4d6cfb35215db3
Daniel Cousens <github@dcousens.com>
2015-07-30 08:49:57 +1000

    eh

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,61 +1,87 @@
 var React = require('react/addons')
-var classNames = require('classnames')
+var classnames = require('classnames')
 
 module.exports = React.createClass({
 	getDefaultProps () {
 		return {
-			days: [10, 28, 29, 30, 31, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15].map((x, i) => {
-				return {
-					name: x,
-					highlighted: false,
-					continues: i === 0 || i === 15,
-					selected: (i >= 0 && i <= 5) || i >= 14 || x === 7 || x === 6,
-					disabled: x === 9
+// 			days: [10, 28, 29, 30, 31, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15].map((x, i) => {
+// 				return {
+// 					name: x,
+// 					highlighted: false,
+// 					selected: (i >= 0 && i <= 5) || i >= 14 || x === 7 || x === 6,
+// 					disabled: x === 9
+// 				}
+// 			}),
+			dayNames: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(x => (
+				{
+					name: x.slice(0, 1),
+					title: x
 				}
-			}),
-			dayNames: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(x => ({ name: x.slice(0, 1), title: x })),
-			months: ['Foo', 'Bar'].map(x => ({ name: x.slice(0, 3), value: x })),
+			)),
+			days: [30, 1, 2, 3].map(x => x.toString()),
+			months: ['February', 'May'].map(x => ({ name: x.slice(0, 3), value: x })),
+			years: ['2003', '2004'],
+
+			disabled: {
+				days: [0, 1, 2, 4]
+			},
+
 			onSelect: () => {},
-			selectedMonth: 'Bay',
-			selectedYear: '2004',
-			years: ['2003', '2004']
+			selected: {
+				year: '2004',
+				month: 'May',
+				days: [-1, 0, 1, 2, 3]
+			}
 		}
+	},
+
+	onSelect (year, month, day) {
+		if (!this.props.onSelect) return
+
+		this.props.onSelect(this.props.
 	},
 
 	render () {
 		var {
-			days,
 			dayNames,
+
+			days,
 			months,
-			onSelect,
-			selectedMonth,
-			selectedYear,
-			years
+			years,
+
+			disabled,
+			highlighted,
+			selected
 		} = this.props
 
-		var renderLegend = dayNames.map((day, i) => {
-			return <abbr className="DateSelectCalendar__legend__day" key={'day-abbr-' + i} title={day.name}>{day.abbr}</abbr>
-		})
+		var disabledMap = {}
+		disabled.forEach(dayIndex => { disabledMap[dayIndex] = true })
 
+		var highlightedMap = {}
+		highlighted.forEach(dayIndex => { highlightedMap[dayIndex] = true })
+
+		var selectedMap = {}
+		selected.forEach(dayIndex => { selectedMap[dayIndex] = true })
+
+		var renderLegend = dayNames.map((day, i) => { return <abbr className="DateSelectCalendar__legend__day" key={'day-abbr-' + i} title={day.name}>{day.abbr}</abbr> })
 		var renderDays = days.map((day, i) => {
-			var handleOnSelect = onSelect && function () {
-				onSelect(day)
-			}
-
-			// range select
-			var selected = day.selected
-			var dayBefore = days[i - 1] || { selected: day.continues }
-			var dayAfter = days[i + 1] || { selected: day.continues }
+			var dayDisabled = disabled[i]
+			var dayHighlighted = highlighted[i]
+			var daySelected = selected[i]
 			var selectClass
 
-			if (selected) {
-				if (dayBefore.selected && dayAfter.selected) {
+			if (daySelected) {
+				var dayBeforeSelected = selectedMap[i - 1]
+				var dayAfterSelected = selectedMap[i + 1]
+
+				// range select
+				if (dayBeforeSelected && dayAfterSelected) {
 					selectClass = 'is-range-selected'
 
-				} else if (dayBefore.selected) {
+				} else if (dayBeforeSelected) {
 					selectClass = 'is-selected is-selected-closed'
 
-				} else if (dayAfter.selected) {
+				} else if (dayAfterSelected) {
 					selectClass = 'is-selected is-selected-open'
 
 				} else {
@@ -63,23 +89,30 @@ module.exports = React.createClass({
 				}
 			}
 
-			var dayClass = classNames('DateSelectCalendar__month__day', selectClass, {
-				'is-highlighted': day.highlighted,
-				'DateSelectCalendar__legend': day.disabled
+			var dayClassName = classnames('DateSelectCalendar__month__day', selectClass, {
+				'is-highlighted': dayHighlighted,
+				'DateSelectCalendar__legend': dayDisabled
 			})
 
-			return <button key={'day-' + i} onClick={handleOnSelect} className={dayClass}>{day}</button>
+			var handleOnSelect
+			if (!dayDisabled) {
+				handleOnSelect = () => {
+					this.onSelect(this.props.selected.year, this.props.selected.month, day)
+				}
+			}
+
+			return <button key={'day-' + i} className={dayClassName} onClick={handleOnSelect}>{day}</button>
 		})
 
-		var monthOptions = months.map((month, i) => { return <option key={'month-' + i} value={month.value}>{month.name}</option> })
+		var monthOptions = months.map((month, i) => { return <option key={'month-' + i} value={month.value}>{month.label}</option> })
 		var yearOptions = years.map((year, i) => { return <option key={'year-' + i} value={year}>{year}</option> })
 
 		return (
 			<div className='DateSelectCalendar'>
 				<div className="DateSelectCalendar__toolbar">
 					<button className="DateSelectCalendar__toolbar__button DateSelectCalendar__toolbar__button--prev">Previous Month</button>
-					<select className="DateSelectCalendar__toolbar__select" value={selectedMonth}>{monthOptions}</select>
-					<select className="DateSelectCalendar__toolbar__select" value={selectedYear}>{yearOptions}</select>
+					<select className="DateSelectCalendar__toolbar__select" value={selected.month}>{monthOptions}</select>
+					<select className="DateSelectCalendar__toolbar__select" value={selected.year}>{yearOptions}</select>
 					<button className="DateSelectCalendar__toolbar__button DateSelectCalendar__toolbar__button--next">Next Month</button>
 				</div>
 				<div className="DateSelectCalendar__legend">{renderLegend}</div>
--- src/Single.js
+++ src/Single.js
@@ -1,16 +1,16 @@
 var React = require('react/addons')
-var moment = require('moment')
-var classNames = require('classnames')
 
-var DateSelectHeader = require('./DateSelectHeader')
+var classnames = require('classnames')
+var moment = require('moment')
+
+var Header = require('./DateSelectHeader')
 
 module.exports = React.createClass({
-	render() {
+	render () {
 		var self = this
 		var firstDayOfMonth = moment().startOf('month').format('D')
 		var lastDayOfMonth  = moment().endOf('month').format('D')
 		var currentDayOfMonth  = moment().format('D')
-
 
 		// variables
 		var currentMonth = moment().format('MMMM')
@@ -21,43 +21,34 @@ module.exports = React.createClass({
 		var daysOfTheMonth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 		var daysOfTheWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 
-		for(var  i = firstDayOfMonth i < lastDayOfMonth i++) {
+		for (var i = firstDayOfMonth; i < lastDayOfMonth; i++) {
 			daysOfTheMonth.push(i)
 		}
-		for (var i = this.props.yearRange[0] i < this.props.yearRange[1] i++) {
-			years.push(i)
+
+		for (var j = this.props.yearRange[0]; j < this.props.yearRange[1]; j++) {
+			years.push(j)
 		}
 
-		var calendarClass = classNames('DateSelectCalendar', {
+		var calendarClass = classnames('DateSelectCalendar', {
 			'DateSelectCalendar--start': this.props.startDate,
 			'DateSelectCalendar--end': this.props.endDate
 		})
 
-
-		// elements
-
-		var weekDays = daysOfTheWeek.map(function(day, i) {
-			return <abbr key={'day' + i} className="DateSelectCalendar__legend__day" title={day}>{day.slice(0,1)}</abbr>
-		})
-		var monthDays = daysOfTheMonth.map(function(day) {
-			var dayClass = classNames('DateSelectCalendar__month__day', {
-				'is-current-day': day == currentDayOfMonth,
-				'is-selected': day == self.state.selectedDate,
+		var weekDays = daysOfTheWeek.map((day, i) => { return <abbr key={'day' + i} className="DateSelectCalendar__legend__day" title={day}>{day.slice(0, 1)}</abbr> })
+		var monthDays = daysOfTheMonth.map((day) => {
+			var dayClass = classnames('DateSelectCalendar__month__day', {
+				'is-current-day': day === currentDayOfMonth,
+				'is-selected': day === self.state.selectedDate,
 				'is-before-selected-day': self.state.selectedDate && (day < self.state.selectedDate),
 				'is-after-selected-day': self.state.selectedDate && (day > self.state.selectedDate)
 			})
+
 			return <button key={'day' + day} onClick={self.handleDaySelection.bind(self, day)} className={dayClass}>{day}</button>
 		})
 
-		var titleMonths = months.map(function(month, i) {
-			return <option key={'month' + i} value={month}>{month.slice(0,3)}</option>
-		})
-		var titleYears = years.map(function(year, i) {
-			return <option key={'year' + i} value={year}>{year}</option>
-		})
-
-		var calendar = <div className={calendarClass}>
-			{!this.props.isHeaderless && <DateSelectHeader selectedDate={this.state.selectedDate} isExpanded={this.props.isExpanded} />}
+		var titleMonths = months.map((month, i) => { return <option key={'month' + i} value={month}>{month.slice(0,3)}</option> })
+		var titleYears = years.map((year, i) => { return <option key={'year' + i} value={year}>{year}</option> })
+		return <div className={calendarClass}>
 			<div className="DateSelectCalendar__toolbar">
 				<button className="DateSelectCalendar__toolbar__button DateSelectCalendar__toolbar__button--prev">Previous Month</button>
 				<select className="DateSelectCalendar__toolbar__select" defaultValue={currentMonth}>{titleMonths}</select>
@@ -68,6 +59,17 @@ module.exports = React.createClass({
 			<div className="DateSelectCalendar__month">{monthDays}</div>
 		</div>
 
-		return calendar
+		return (
+			<Header selectedDate={this.state.selectedDate} isExpanded={this.props.isExpanded} />
+			<Calendar
+				days={days}
+				dayNames={dayNames}
+				months={months}
+				disabled={disabledOptions}
+
+				onSelect={onSelect}
+				selected={selected}
+			/>
+		)
 	}
 })
