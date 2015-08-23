a2d7dc819bf090f0eace0ca579659c820a42d771
Jed Watson <jed.watson@me.com>
2015-08-23 22:44:09 +1000

    Adding wrapper + styles for inline Calendar use

--- src/DateSelect.js
+++ src/DateSelect.js
@@ -1,2 +1,24 @@
+var blacklist = require('blacklist');
+var CalendarComponent = require('./Calendar');
+var classNames = require('classnames');
+var React = require('react/addons');
+
 export var Popup = require('./Popup');
-export var Calendar = require('./Calendar');
+
+var InlineCalendar = React.createClass({
+	propTypes: {
+		className: React.PropTypes.string,
+		style: React.PropTypes.object
+	},
+	render () {
+		var calendarProps = blacklist(this.props, 'className', 'style');
+		var componentClass = classNames('DateSelect DateSelect-inline-container', this.props.className);
+		return (
+			<div className={componentClass} style={this.props.style}>
+				<CalendarComponent {...calendarProps} />
+			</div>
+		);
+	}
+});
+
+export var Calendar = InlineCalendar;
